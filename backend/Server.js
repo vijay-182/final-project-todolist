e50b299293beabbbed80733627ea7d845c5ac118
VIJAY SV <117309043+vijay-182@users.noreply.github.com>
2023-02-28 21:30:14 +0530

    added new files1

---
+++ backend/Server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/ToDoRoute");
+require('dotenv').config();
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cors());
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Mongodb Connected..."))
+    .catch((err) => console.error(err));
+app.use(routes);
+
+app.listen(PORT, () => console.log("Server running on port " + PORT));
---
+++ backend/routes/ToDoRoute.js
@@ -0,0 +1,19 @@
+const { Router } = require("express");
+
+const { getToDo, saveToDo, deleteToDo, updateToDo } = require("../controllers/ToDoController");
+
+const router = Router();
+
+router.get('/',(req,res)=>{
+res.json({message:"Hi there"})
+})
+
+router.get("/", getToDo);
+
+router.post("/save", saveToDo);
+
+router.post("/update", updateToDo);
+
+router.post("/delete", deleteToDo);
+
+module.exports = router;
